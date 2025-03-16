b009d9438417cc78a76adc069fbc5fbe65489cdd
Miguel Colque <micky.cm@outlook.es>
2025-03-16 19:55:16 -0400

    Creada la aplicación products con sus modelos, vistas y templates

--- coffeeshop/settings.py
+++ coffeeshop/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for coffeeshop project.
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'coffeeshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
